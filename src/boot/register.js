723dd924fc105e20f0d7fdd5d72d5f0073eaef5c
Emanuele Bertoldi <emanuele.bertoldi@gmail.com>
2021-10-18 18:35:44 +0200

    feat: add RbResourceLink

--- src/boot/register.js
+++ src/boot/register.js
@@ -1,9 +1,11 @@
 import RbActionMenu from '../components/RbActionMenu.vue'
 import RbColumnAutoFilter from '../components/RbColumnAutoFilter.vue'
 import RbDataTable from '../components/RbDataTable.vue'
+import RbResourceLink from '../components/RbResourceLink.vue'
 
 export default ({ app }) => {
   app.component(RbActionMenu.name, RbActionMenu)
   app.component(RbColumnAutoFilter.name, RbColumnAutoFilter)
   app.component(RbDataTable.name, RbDataTable)
+  app.component(RbResourceLink.name, RbResourceLink)
 }
