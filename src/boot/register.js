dbf3e79d3e67e77dac38eed5321369973eddaf86
Emanuele Bertoldi <emanuele.bertoldi@gmail.com>
2021-10-27 22:07:57 +0200

    feat: add RbResourceTablePage

--- src/boot/register.js
+++ src/boot/register.js
@@ -7,6 +7,7 @@ import RbSidebar from '../components/RbSidebar.vue'
 import RbFormWrapper from '../components/RbFormWrapper.vue'
 import RbMainLayout from '../layouts/RbMainLayout.vue'
 import RbMasterDetailPage from '../pages/RbMasterDetailPage.vue'
+import RbResourceTablePage from '../pages/RbResourceTablePage.vue'
 
 export default ({ app }) => {
   app.component(RbContainer.name, RbContainer)
@@ -18,4 +19,5 @@ export default ({ app }) => {
   app.component(RbFormWrapper.name, RbFormWrapper)
   app.component(RbMainLayout.name, RbMainLayout)
   app.component(RbMasterDetailPage.name, RbMasterDetailPage)
+  app.component(RbResourceTablePage.name, RbResourceTablePage)
 }
---
+++ src/pages/RbResourceTablePage.vue
@@ -0,0 +1,36 @@
+<template>
+  <q-page class="flex justify-center">
+    <rb-container>
+      <rb-resource-collection :resource="resource" v-slot="props">
+        <slot name="table" v-bind="{ ...$props, ...$attrs }">
+          <rb-data-table
+            class="full-width"
+            :title="props.resource.label"
+            :columns="props.resource.columns"
+            :rows="props.items"
+            :actions="props.resource.actions"
+            @row-click="onRowClicked"
+          />
+        </slot>
+      </rb-resource-collection>
+    </rb-container>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "RbResourceTablePage",
+
+  props: {
+    resource: Object,
+  },
+
+  methods: {
+    onRowClicked(evt, row) {
+      this.$emit('click-row', row);
+    }
+  },
+});
+</script>
