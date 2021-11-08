df864eeab215854827c815a7fe0ce3aa77172580
Emanuele Bertoldi <emanuele.bertoldi@gmail.com>
2021-11-08 19:09:11 +0100

    chore: lint

--- src/index.js
+++ src/index.js
@@ -6,27 +6,27 @@
  * API: https://github.com/quasarframework/quasar/blob/master/app/lib/app-extension/IndexAPI.js
  */
 
- function extendConf (conf) {
-    // make sure necessary plugins are injected:
-    conf.framework.plugins.push('Dialog')
+function extendConf (conf) {
+  // make sure necessary plugins are injected:
+  conf.framework.plugins.push('Dialog')
 
-    // make sure boot file is registered
-    conf.boot.push('~quasar-app-extension-rb-ui/src/boot/register.js')
-  
-    // make sure boot & component files get transpiled
-    conf.build.transpileDependencies.push(/quasar-app-extension-rb-ui[\\/]src/)
-  }
-  
-  module.exports = function (api) {
-    // (Optional!)
-    // Quasar compatibility check; you may need
-    // hard dependencies, as in a minimum version of the "quasar"
-    // package or a minimum version of "@quasar/app" CLI
-    api.compatibleWith('quasar', '^2.0.0-beta.19')
-    api.compatibleWith('@quasar/app', '^3.0.0-beta.28')
-  
-    // Here we extend /quasar.conf.js, so we can add
-    // a boot file which registers our new UI component;
-    // "extendConf" will be defined below (keep reading the tutorial)
-    api.extendQuasarConf(extendConf)
-  }
+  // make sure boot file is registered
+  conf.boot.push('~quasar-app-extension-rb-ui/src/boot/register.js')
+
+  // make sure boot & component files get transpiled
+  conf.build.transpileDependencies.push(/quasar-app-extension-rb-ui[\\/]src/)
+}
+
+module.exports = function (api) {
+  // (Optional!)
+  // Quasar compatibility check; you may need
+  // hard dependencies, as in a minimum version of the "quasar"
+  // package or a minimum version of "@quasar/app" CLI
+  api.compatibleWith('quasar', '^2.0.0-beta.19')
+  api.compatibleWith('@quasar/app', '^3.0.0-beta.28')
+
+  // Here we extend /quasar.conf.js, so we can add
+  // a boot file which registers our new UI component;
+  // "extendConf" will be defined below (keep reading the tutorial)
+  api.extendQuasarConf(extendConf)
+}
