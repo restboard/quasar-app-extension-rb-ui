94c3da13f185324b0fe65e8f007767d4cde41098
Emanuele Bertoldi <emanuele.bertoldi@gmail.com>
2022-08-06 09:32:43 +0200

    feat: make RbSelect filterable

--- src/components/RbSelect.vue
+++ src/components/RbSelect.vue
@@ -1,7 +1,9 @@
 <template>
   <q-select
+    use-input
     :options="options"
     :loading="loading"
+    @filter="onFilter"
   />
 </template>
 
@@ -33,10 +35,25 @@ export default defineComponent({
 
   computed: {
     options () {
-      return (this.items || []).map(item => ({
-        value: item[this.valueKey],
-        label: item[this.labelKey]
-      }))
+      return (this.items || [])
+        .map(item => ({
+          value: item[this.valueKey],
+          label: item[this.labelKey]
+        }))
+        .filter(item => !this.query || item.label.toLowerCase().indexOf(this.query) > -1)
+    }
+  },
+
+  data () {
+    return {
+      query: ''
+    }
+  },
+
+  methods: {
+    onFilter (val, update) {
+      const needle = (val || '').toLowerCase()
+      update(() => { this.query = needle })
     }
   }
 })
